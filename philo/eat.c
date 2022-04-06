3ac981f0831fd2454a199716ce89bc65961639f3
AlecLavallee <43756976+AlecLavallee@users.noreply.github.com>
2022-04-06 03:12:47 +0200

    Eat function added.

--- philo/eat.c
+++ philo/eat.c
@@ -11,3 +11,30 @@
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+void	philo_eat(t_philo *philo)
+{
+	t_share	*share;
+
+	share = philo->share;
+	pthread_mutex_lock(&philo->fork);
+	print_status(philo, 0);
+	if (philo->share->nb_philos == 1)
+	{
+		add_sleep(data, data->t_die);
+		print_status(philo, 4);
+		pthread_mutex_unlock(&philo->fork);
+		data->is_dead = 1;
+		return ;
+	}
+	pthread_mutex_lock(&philo->prev->fork);
+	print_status(philo, 0);
+	pthread_mutex_lock(&share->check);
+	philo->dining++;
+	print_status(philo, 1);
+	philo->last_dine = get_time();
+	pthread_mutex_unlock(&share->check);
+	add_sleep(share, share->time_to_eat);
+	pthread_mutex_unlock(&philo->fork);
+	pthread_mutex_unlock(&philo->prev->fork);
+}
