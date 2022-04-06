9c0a85753bf2d9ec938b2a5c52c7778b900e4b24
alelaval <alelaval@student.42.fr>
2022-04-06 21:07:54 +0200

    fixed freeing of the program.

--- philo/close.c
+++ philo/close.c
@@ -6,7 +6,7 @@
 /*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 15:44:20 by alelaval          #+#    #+#             */
-/*   Updated: 2022/04/06 16:19:45 by alelaval         ###   ########.fr       */
+/*   Updated: 2022/04/06 18:24:22 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,13 @@
 void	exit_philo(t_share *share, pthread_t *t_id)
 {
 	int		i;
-	t_philo	*next;
 
-	i = 0;
-	while (i < share->nb_philos)
-	{
+	i = -1;
+	while (++i < share->nb_philos)
 		pthread_join(t_id[i], NULL);
-		i++;
-	}
-	next = NULL;
-	while (share->philos->next)
-	{
-		next = share->philos->next;
-		free(share->philos);
-	}
-	free (next);
+	i = -1;
+	while (++i < share->nb_philos)
+		pthread_mutex_destroy(&share->philos[i].fork);
+	pthread_mutex_destroy(&share->print);
+	pthread_mutex_destroy(&share->check);
 }
