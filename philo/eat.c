a37979cab2a0eda49466652c8449e139016ac904
alelaval <alelaval@student.42.fr>
2022-04-06 21:08:48 +0200

    Modified sequence to avoid locking when only one philo.

--- philo/eat.c
+++ philo/eat.c
@@ -6,27 +6,32 @@
 /*   By: alelaval <alelaval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 19:56:01 by alelaval          #+#    #+#             */
-/*   Updated: 2022/04/06 15:31:48 by alelaval         ###   ########.fr       */
+/*   Updated: 2022/04/06 20:19:42 by alelaval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+/* 
+**philo tries to take his/her fork in its hand
+** and the one from the previous philosopher
+** after philo gets to sleep
+*/
 
 void	philo_eat(t_philo *philo)
 {
 	t_share	*share;
 
 	share = philo->share;
-	pthread_mutex_lock(&philo->fork);
-	print_status(philo, 0);
 	if (philo->share->nb_philos == 1)
 	{
 		add_sleep(share, share->time_to_die);
 		print_status(philo, 4);
-		pthread_mutex_unlock(&philo->fork);
 		share->is_dead = 1;
 		return ;
 	}
+	pthread_mutex_lock(&philo->fork);
+	print_status(philo, 0);
 	pthread_mutex_lock(&philo->prev->fork);
 	print_status(philo, 0);
 	pthread_mutex_lock(&share->check);
